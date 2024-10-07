a7c58b0c39da10153d730d8660b6e40ef21f2792
Shravastee Thakur <shravastee@gmail.com>
2024-10-07 21:38:12 +0530

    Database connected

--- src/db/index.js
+++ src/db/index.js
@@ -3,9 +3,17 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(
+      `MONGODB connected !! DB_HOST : ${connectionInstance.connection.host}`
+    );
   } catch (error) {
     console.log("MONGODB connection failed", error);
     process.exit(1);
   }
 };
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+connectDB();
