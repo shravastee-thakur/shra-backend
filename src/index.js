e7a994af35aa707240c06ea6fe43a8b154aacc55
Shravastee Thakur <shravastee@gmail.com>
2024-10-08 21:38:39 +0530

    Express Server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed", err);
+  });
