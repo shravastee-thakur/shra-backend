8d77c7acfbaf31cd36e472e9e6ff81bfb5b3f689
Shravastee Thakur <shravastee@gmail.com>
2024-10-29 21:53:33 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -16,4 +16,10 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+// ROUTES IMPORT
+import userRouter from "./routes/user.routes.js";
+
+// ROUTES declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
